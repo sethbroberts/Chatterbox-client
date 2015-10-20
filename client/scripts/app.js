6f57ea375ee34d81bbb6b261470005d4ca87f0a2
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-10-20 17:26:45 -0400

    passing requirements, most functions present

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,44 +1,23 @@
 // YOUR CODE HERE:
 
-$(document).ready(function() {
-
-});
 
 var app = (function(){
 
   var serverName = "https://api.parse.com/1/classes/chatterbox";
   var messageTemplate = '<div class="chat"><span class="username">$username: </span>$text</div>';
   var friends = {};
-  var rooms = { lobby: "lobby" };
   var currentRoom = "lobby";
-
-  var updateMessages = function (data) {
-    //grab chat window, clear existing messages
-    var $chatWindow = $('#chats');
-    $chatWindow.html('');
-
-    //iterate through messages, append to chat window
-    //go backward through array, since most recent message is at end of array
-    for (var i = 0; i < data.results.length; i++) {
-      addMessage(data.results[i]);
-      rooms[data.results[i].roomname] = data.results[i].roomname;
-    }
-
-    for (var each in rooms) {
-      addRoom(each);
-    }
-
-  };
 
   var logError = function (data) {
     console.error("chatterbox: Failed to send message. Error: ", data);
   }
 
-  var fetch = function () {
+  var fetch = function (callback) {
     $.ajax({
       url: "https://api.parse.com/1/classes/chatterbox",
       type: "GET",
-      success: updateMessages,
+      data: "order=-createdAt",
+      success: callback,
       error: logError
     });
   }
@@ -65,7 +44,7 @@ var app = (function(){
     var $chatWindow = $('#chats');
     var output = messageTemplate.replace('$username', message.username);
     output = output.replace('$text', message.text);
-    $chatWindow.prepend(output);
+    $chatWindow.append(output);
     var $username = $('.username:first');
     // set event handler on $username, 2nd argument is event.data object
     // need 'this.addFriend' -> this will call the method of app instance, as opposed to just function
@@ -74,8 +53,9 @@ var app = (function(){
 
   var addRoom = function(roomName) {
     var $roomSelect = $('#roomSelect');
-    var optionTemplate = '<option value="$roomName">$roomName</option>';
-    var output = optionTemplate.replace(/\$roomName/g, roomName);
+    var optionTemplate = '<option class="room" id="$id">$roomName</option>';
+    var output = optionTemplate.replace('$roomName', roomName);
+    output = output.replace('$id', roomName.replace(/\s/g, '_'))
     $roomSelect.append(output);
   };
 
@@ -96,14 +76,24 @@ var app = (function(){
       username: username,
       text: text,
       roomname: currentRoom
-    })
+    });
 
     event.preventDefault();
 
   };
 
+  var changeRoom = function (roomName) {
+    app.currentRoom = roomName;
+    roomName = roomName.replace(/\s/g, '_')
+    $('#' + roomName).attr('selected', 'selected');
+  }
+
   var init = function() {
-    $('#send .submit').submit(app.handleSubmit);
+    $('#send .submit').click(app.handleSubmit);
+    $select = $('select');
+    $select.change(function() {
+      app.changeRoom($(this).val());
+    });
   };
 
   return { 
@@ -115,7 +105,65 @@ var app = (function(){
     addMessage: addMessage,
     addRoom: addRoom,
     addFriend: addFriend,
-    handleSubmit: handleSubmit
+    handleSubmit: handleSubmit,
+    currentRoom: currentRoom,
+    changeRoom: changeRoom
   }
 
 }())
+
+$(document).ready(function() {
+  var rooms = { lobby: "lobby" };
+
+  var charEscapes = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#x27;',     
+    '/': '&#x2F;'
+  };
+
+  var updateMessages = function (data) {
+    //grab chat window, clear existing messages
+    app.clearMessages();
+    $('#roomSelect').html('');
+
+    if (app.currentRoom !== 'lobby') {
+      data.results = data.results.filter(function(message) {
+        return message.roomname === app.currentRoom;
+      });
+    }
+
+    //iterate through messages, append to chat window
+    //go backward through array, since most recent message is at end of array
+    for (var i = 0; i < data.results.length; i++) {
+      var escape = function(ch) { return charEscapes[ch] || ch; };
+      var message = data.results[i].text || '';
+      data.results[i].text = message.split('').map(escape).join('');
+      app.addMessage(data.results[i]);
+      rooms[data.results[i].roomname] = data.results[i].roomname;
+    }
+
+    for (var each in rooms) {
+      app.addRoom(each);
+    }
+
+    app.changeRoom(app.currentRoom);
+  };
+
+  var fetchMessages = function() {
+    app.fetch(updateMessages);
+    setTimeout(fetchMessages, 1000);
+  }
+
+  app.init();
+
+  $('#addRoom').click(function() {
+    var room = prompt('Enter room name:');
+    rooms[room] = room;
+    app.changeRoom(room);
+  });
+
+  fetchMessages();
+});
